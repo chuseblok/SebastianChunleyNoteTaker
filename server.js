fab586c58a3c26841fa0089af55f9b37d8a7b81f
sebastian Chunley <101592116+chuseblok@users.noreply.github.com>
2022-05-23 09:30:08 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiroutes');
+const htmlroutes = require('./routes/htmlroutes');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001; 
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true }));
+
+app.use(express.static('public'));
+
+app.use('/apiRoutes', apiRoutes);
+
+app.use('/', htmlroutes);
+
+app.listen(PORT);
