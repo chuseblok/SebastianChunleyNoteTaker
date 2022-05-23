c94d3951523e570c340a8f86d9f91378c3c03381
sebastian Chunley <sebastian.chunley@gmail.com>
2022-05-23 10:05:56 -0400

    g

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const apiRoutes = require('./routes/apiroutes');
-const htmlroutes = require('./routes/htmlroutes');
+const express = require("express");
+const apiRoutes = require("./routes/apiroutes");
+const htmlroutes = require("./routes/htmlroutes");
 
 const app = express();
 
-const PORT = process.env.PORT || 3001; 
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
-app.use(express.urlencoded({extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use('/apiRoutes', apiRoutes);
+app.use("/apiRoutes", apiRoutes);
 
-app.use('/', htmlroutes);
+app.use("/", htmlroutes);
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log("localhost/3001");
+});
